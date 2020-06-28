95eba1b0d9627705258fba45fa8003484edb4c39
Kiran Challa <kichalla@microsoft.com>
2020-06-28 08:50:38 -0700

    updated

--- images/build/dynamicInstallScripts/installPython.sh
+++ images/build/dynamicInstallScripts/installPython.sh
@@ -6,7 +6,7 @@ PARAMS=""
 while (( "$#" )); do
   case "$1" in
     -d|--dir)
-      targetDir=$2
+      parentDir=$2
       shift 2
       ;;
     -p|--platform)
@@ -15,6 +15,10 @@ while (( "$#" )); do
       ;;
     -v|--version)
       version=$2
+      shift 2
+      ;;
+    -u|--base-url)
+      storageBaseUrl=$2
       shift 2
       ;;
     --) # end argument parsing
@@ -34,22 +38,33 @@ done
 # set positional arguments in their proper place
 eval set -- "$PARAMS"
 
-PLATFORM_NAME="$1"
-VERSION="$2"
+function printUsage() {
+    echo "Usage: installPlatform.sh -p <platform-name> -v <platform-version> -d <directory-to-install> -u <storage-url>"
+    exit 1
+}
 
-fileName="$PLATFORM_NAME-$VERSION.tar.gz"
-platformDir="/opt/$PLATFORM_NAME"
-
-if [ -z "$targetDir" ]; then
-    targetDir="$platformDir/$VERSION"
+defaultInstallationRootDir="/tmp/oryx/platforms"
+if [ -z "$platform" ]; then
+    echo "Platform name is required."
+    printUsage
 fi
-#--------------------------------
 
-platform="$1"
-version="$2"
-installationParentDir="$3"
+if [ -z "$version" ]; then
+    echo "Platform version is required."
+    printUsage
+fi
 
-fullInstallationPath="$installationParentDir/$version"
+if [ -z "$parentDir" ]; then
+    parentDir="$defaultInstallationRootDir/$platform/$version"
+fi
+
+if [ -z "$storageBaseUrl" ]; then
+    storageBaseUrl="https://oryxsdksdev.blob.core.windows.net"
+fi
+
+fileName="$platform-$version.tar.gz"
+fullInstallationPath="$parentDir/$version"
+
 PLATFORM_SETUP_START=$SECONDS
 echo
 echo "Downloading and extracting '$platform' version '$version' to '$fullInstallationPath'..."
@@ -59,22 +74,22 @@ cd "$fullInstallationPath"
 PLATFORM_BINARY_DOWNLOAD_START=$SECONDS
 curl \
     -D headers.txt \
-    -SL "https://oryxsdksdev.blob.core.windows.net/python/python-3.8.3.tar.gz" \
-    --output 3.8.3.tar.gz \
+    -SL "$storageBaseUrl/$platform/$fileName" \
+    --output "$version.tar.gz" \
     >/dev/null 2>&1
 PLATFORM_BINARY_DOWNLOAD_ELAPSED_TIME=$(($SECONDS - $PLATFORM_BINARY_DOWNLOAD_START))
 echo "Downloaded in $PLATFORM_BINARY_DOWNLOAD_ELAPSED_TIME sec(s)."
-echo Verifying checksum...
+echo "Verifying checksum..."
 headerName="x-ms-meta-checksum"
 checksumHeader=$(cat headers.txt | grep -i $headerName: | tr -d '\r')
 checksumHeader=$(echo $checksumHeader | tr '[A-Z]' '[a-z]')
 checksumValue=${checksumHeader#"$headerName: "}
 rm -f headers.txt
-echo "$checksumValue 3.8.3.tar.gz" | sha512sum -c - >/dev/null 2>&1
+echo "$checksumValue $version.tar.gz" | sha512sum -c - >/dev/null 2>&1
 echo Extracting contents...
-tar -xzf 3.8.3.tar.gz -C .
-rm -f 3.8.3.tar.gz
+tar -xzf $version.tar.gz -C .
+rm -f $version.tar.gz
 PLATFORM_SETUP_ELAPSED_TIME=$(($SECONDS - $PLATFORM_SETUP_START))
 echo "Done in $PLATFORM_SETUP_ELAPSED_TIME sec(s)."
 echo
-echo > /tmp/oryx/platforms/python/3.8.3/.oryx-sdkdownload-sentinel
+echo > $fullInstallationPath/.oryx-sdkdownload-sentinel
